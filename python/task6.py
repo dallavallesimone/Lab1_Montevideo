16085fc29faa1438e169f84d7a70b9b127183f88
massibelluomini <46016846+massibelluomini@users.noreply.github.com>
2020-11-08 17:03:00 +0100

    C identity matrix

--- python/task6.py
+++ python/task6.py
@@ -43,7 +43,43 @@ for i in range(0, matsize):
         A[j][i]=int(a[j])
         B[j][i]=int(b[j])
 
-print("\n---A---\n")
+"""print("\n---A---\n")
 print(A)
 print("\n---B---\n")
-print(B)
+print(B)"""
+
+det_A = np.linalg.det(A)
+A_inv = np.linalg.inv(A) 
+Ai = np.rint(A_inv * det_A) % 2
+
+
+with open(r"C:\Users\massi\Downloads\Lab1_Montevideo_2\KPApairsMontevideo_linear.hex", 'r') as f:
+    lines = f.read().split('\n')
+# Get x and y values from each line and append to self.data
+spec = '{fill}{align}{width}{type}'.format(fill='0', align='>', width=32, type='b')
+for line in lines:
+    sample = line.split('\t')
+    plaintext = str(format(int(sample[0], 16),spec))
+    ciphertext = str(format(int(sample[1], 16),spec))
+
+    plaintext = [plaintext[i:i+1] for i in range(0, len(plaintext), 1)]
+    plaintext = [int(item) for item in plaintext]
+    plaintext = np.array(plaintext)
+
+    ciphertext = [ciphertext[i:i+1] for i in range(0, len(ciphertext), 1)]
+    ciphertext = [int(item) for item in ciphertext]
+    ciphertext = np.array(ciphertext)
+
+k = np.dot(Ai, ciphertext^np.dot(B, plaintext)) % 2
+key = 0x00000000
+for i in range(0, 32):
+    key = key ^ (int(k[i]) << (32 - i))
+     
+print("k:", hex(key))
+
+#Check plaintext 6766C94F	76BC0A0D
+x=encryptTask5( 0x1ee97f91e, 32, 0x6766C94F, 32, 5)
+print("\nx computed: " + str(hex(x))+ "\n")
+print("x given: 0x76BC0A0D\n")
+
+
